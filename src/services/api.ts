28393fd28ee3df6c5df2e2bf23a261e04dc5f59e
Mateus Fugita <mateushiwao@gmail.com>
2021-10-10 12:40:00 -0300

    feat: set env var for back-end url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://localhost:44393/v0',
+    baseURL: process.env.REACT_APP_API_URL || 'https://localhost:44393/v0',
     headers: {'Content-Type' : 'application/json'}
 })
 
